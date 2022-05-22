1afb75501654410ae87d0496b89f630d664ac799
unknown <lfrackowiak4@gmail.com>
2022-05-22 15:59:51 +0200

    na colabie dziala

--- main.py
+++ main.py
@@ -1,172 +1,77 @@
+
 import ast
 n=int(input())
 
-
 class Biblioteka:
-  czytelnicy=[]
+  ksiazki=[]
   egzemplarze=[]
-  wypozyczenia=[]
 
 class Ksiazka:
-    def __init__(self,data):
-        self.tytul=data[0]
-        self.autor=data[1]
-        self.rok=data[2]
-
+    def __init__(self,x):
+        self.nazwa=x[0]
+        self.autor=x[1]
+        self.rok=x[2]
+#akcja_dodania_ksiazki_do_biblioteki
     def dodaj(self):
-        if str((self.tytul, self.autor)) in egzemplarze:
-            egzemplarze[str((self.tytul, self.autor))]+=1
+        if str((self.nazwa,self.autor))in egzemplarze.keys():
+            egzemplarze[str((self.nazwa,self.autor))]+=1
         else:
-            egzemplarze[str((self.tytul, self.autor))]=1
+            egzemplarze[str((self.nazwa,self.autor))]=1
         return True
 
-
-
 class Czytelnik:
-    def __init__(self, imie_nazwisko):
-        self.imie_nazwisko = imie_nazwisko
-
-    def __init__(self,tytul,ksiazka):
-        self.tytul=tytul
+    def __init__(self,nazwa,ksiazka):
+        self.nazwa=nazwa
         self.ksiazka=ksiazka
-
-    def czy_dostepne(self):
-        if self.tytul in czytelnicy:
-            return True
-        else:
-            return False
-
+#sprawdzenie_mozliwosci_wypozyczenia
     def mozliwosc_wypozyczenia(self):
-        if (len(czytelnicy[self.tytul])<3) and (self.ksiazka not in czytelnicy[self.tytul]):
+        if (len(czytelnicy[self.nazwa])<3) and (self.ksiazka not in czytelnicy[self.nazwa]):
             return True
         else:
             return False
-    
+#sprawdzenie_czy_ksiazka_dostepna
+    def czy_dostepne(self):
+        if self.nazwa in czytelnicy.keys():
+            return True
+        else:
+            return False
+#akcja wypozyczenia
     def wypozycz(self):
-        if self.ksiazka in [eval(x)[0] for x in egzemplarze]:
+        if self.ksiazka in [eval(x)[0] for x in egzemplarze.keys()]:
             for i,x in egzemplarze.items():
-                if eval(i)[0] == self.ksiazka:
+                if eval(i)[0]==self.ksiazka:
                     if egzemplarze[i]>0:
                         egzemplarze[i]-=1
-                        czytelnicy[self.tytul].append(self.ksiazka)
+                        czytelnicy[self.nazwa].append(self.ksiazka)
             return True
         else:
             return False
-    
+#akcja oddania
     def odddaj(self):
-        if self.tytul in czytelnicy:
-            if self.ksiazka in czytelnicy[self.tytul]:
-                czytelnicy[self.tytul].remove(self.ksiazka)
+        if self.nazwa in czytelnicy.keys():
+            if self.ksiazka in czytelnicy[self.nazwa]:
+                czytelnicy[self.nazwa].remove(self.ksiazka)
                 for i,x in egzemplarze.items():
-                    if eval(i)[0] == self.ksiazka:
+                    if eval(i)[0]==self.ksiazka:
                         egzemplarze[i]+=1
             return True
         else:
             return False
-            
-
-
-for index in range(0, n):
-    input = input().replace('\r', '').replace('\n', '').replace('(', '').replace(')', '').replace(' "', '').replace('"', '').split(",")
-    akcja=input[0]
-
-
-
-    if akcja[0]=='dodaj':
-        print(Ksiazka(akcja[1:]).dodaj())     
-    if akcja[0] == 'wypozycz':
-        if akcja[1] not in czytelnicy.keys():
-            czytelnicy[akcja[1]] = []
-        if Czytelnik(akcja[1],akcja[2]).mozliwosc_wypozyczenia() == False:
-            print(Czytelnik(akcja[1],akcja[2]).mozliwosc_wypozyczenia())
-        else:
-            print(Czytelnik(akcja[1],akcja[2]).wypozycz())  
-    if akcja[0] == 'oddaj':
-        print(Czytelnik(akcja[1],akcja[2]).odddaj())
         
-import ast
-n=int(input())
+czytelnicy={}
+egzemplarze={}
 
-
-class Biblioteka:
-  czytelnicy=[]
-  egzemplarze=[]
-  wypozyczenia=[]
-
-class Ksiazka:
-    def __init__(self,data):
-        self.tytul=data[0]
-        self.autor=data[1]
-        self.rok=data[2]
-
-    def dodaj(self):
-        if str((self.tytul, self.autor)) in egzemplarze:
-            egzemplarze[str((self.tytul, self.autor))]+=1
+for i in range(n):
+    action=eval(input())
+    action = tuple([str(x).strip() for x in action])
+    if action[0]=='dodaj':
+        print(Ksiazka(action[1:]).dodaj()) 
+    if action[0]=='wypozycz':
+        if action[1] not in czytelnicy.keys():
+            czytelnicy[action[1]]=[]
+        if Czytelnik(action[1],action[2]).mozliwosc_wypozyczenia()==False:
+            print(Czytelnik(action[1],action[2]).mozliwosc_wypozyczenia())
         else:
-            egzemplarze[str((self.tytul, self.autor))]=1
-        return True
-
-
-
-class Czytelnik:
-    def __init__(self, imie_nazwisko):
-        self.imie_nazwisko = imie_nazwisko
-
-    def __init__(self,tytul,ksiazka):
-        self.tytul=tytul
-        self.ksiazka=ksiazka
-
-    def czy_dostepne(self):
-        if self.tytul in czytelnicy:
-            return True
-        else:
-            return False
-
-    def mozliwosc_wypozyczenia(self):
-        if (len(czytelnicy[self.tytul])<3) and (self.ksiazka not in czytelnicy[self.tytul]):
-            return True
-        else:
-            return False
-    
-    def wypozycz(self):
-        if self.ksiazka in [eval(x)[0] for x in egzemplarze]:
-            for i,x in egzemplarze.items():
-                if eval(i)[0] == self.ksiazka:
-                    if egzemplarze[i]>0:
-                        egzemplarze[i]-=1
-                        czytelnicy[self.tytul].append(self.ksiazka)
-            return True
-        else:
-            return False
-    
-    def odddaj(self):
-        if self.tytul in czytelnicy:
-            if self.ksiazka in czytelnicy[self.tytul]:
-                czytelnicy[self.tytul].remove(self.ksiazka)
-                for i,x in egzemplarze.items():
-                    if eval(i)[0] == self.ksiazka:
-                        egzemplarze[i]+=1
-            return True
-        else:
-            return False
-            
-
-
-for index in range(0, n):
-    input = input().replace('\r', '').replace('\n', '').replace('(', '').replace(')', '').replace(' "', '').replace('"', '').split(",")
-    akcja=input[0]
-
-
-
-    if akcja[0]=='dodaj':
-        print(Ksiazka(akcja[1:]).dodaj())     
-    if akcja[0] == 'wypozycz':
-        if akcja[1] not in czytelnicy.keys():
-            czytelnicy[akcja[1]] = []
-        if Czytelnik(akcja[1],akcja[2]).mozliwosc_wypozyczenia() == False:
-            print(Czytelnik(akcja[1],akcja[2]).mozliwosc_wypozyczenia())
-        else:
-            print(Czytelnik(akcja[1],akcja[2]).wypozycz())  
-    if akcja[0] == 'oddaj':
-        print(Czytelnik(akcja[1],akcja[2]).odddaj())
-        
+            print(Czytelnik(action[1],action[2]).wypozycz())   
+    if action[0]=='oddaj':
+        print(Czytelnik(action[1],action[2]).odddaj())
